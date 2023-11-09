432a4162cca89a1fa4a6574d8352b7a12a1e40cc
ElindTahir <if21b033@technikum-wien.at>
2023-11-09 02:17:38 +0100

    Sprint 1

--- NPaperless.Services/Program.cs
+++ NPaperless.Services/Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-app.MapGet("/", () => "Hello World!");
+namespace NPaperless.Services
+{
+    /// <summary>
+    /// Program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-app.Run();
+        /// <summary>
+        /// Create the host builder.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>IHostBuilder</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                   webBuilder.UseStartup<Startup>()
+                             .UseUrls("http://0.0.0.0:8081/");
+                });
+    }
+}
