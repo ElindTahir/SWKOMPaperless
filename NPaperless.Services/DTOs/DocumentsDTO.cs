36d16969db1ee88b5233023feb592f4bf0098091
ElindTahir <95304227+ElindTahir@users.noreply.github.com>
2023-11-11 20:19:49 +0100

    test branch push

---
+++ NPaperless.Services/DTOs/DocumentsDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace NPaperless.Services.DTOs;
+
+public class DocumentsDTO
+{
+    public class NewDocument
+    {
+        public string Title { get; set; }
+        // Include other properties that are needed when creating a new document
+    }
+        
+    public class UpdateDocument
+    {
+        public string Title { get; set; }
+        // Include other properties that are needed when updating an existing document
+    }
+}
